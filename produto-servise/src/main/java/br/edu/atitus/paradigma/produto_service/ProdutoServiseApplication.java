cd54fafb0ea354a0b95f096c9b44cfa992f757e5
Maria Eduarda Carvalho <134561869+Maducdornelles@users.noreply.github.com>
2024-09-19 23:29:34 -0300

    add new updates

---
+++ produto-servise/src/main/java/br/edu/atitus/paradigma/produto_service/ProdutoServiseApplication.java
@@ -0,0 +1,13 @@
+package br.edu.atitus.paradigma.produto_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutoServiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutoServiseApplication.class, args);
+	}
+
+}
